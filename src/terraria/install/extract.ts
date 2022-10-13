7122e7cf200e70cb6e3318d2d0116fe6781cfd2a
Joakim Isaksen <pvpdaimer@gmail.com>
2022-10-13 17:49:28 +0200

    Small changes

--- src/terraria/install/extract.ts
+++ src/terraria/install/extract.ts
@@ -1,16 +1,16 @@
 import { join } from 'node:path';
+import { chmod } from 'node:fs/promises';
+
 import AdmZip from 'adm-zip';
 
-import { versionsDir } from '@terraria';
+import { versionsDir, serversDir } from '@terraria';
 
-export default function extractVersion(v: string): Promise<void> {
+export default async function extractVersion(v: string): Promise<void> {
     const versionPath = join(versionsDir, v);
     const fileName = versionPath + '.zip';
 
-    return new Promise(resolve => {
-        const zip = new AdmZip(fileName);
-
-        zip.extractEntryTo(v + '/Linux/', versionPath, false, false, true);
-        resolve();
-    });
+    const zip = new AdmZip(fileName);
+    zip.extractEntryTo(v + '/Linux/', versionPath, false, false, true);
+    
+    await chmod(join(serversDir, v, 'TerrariaServer.bin.86_64'), 0o755);
 }
--- src/terraria/run.sh
+++ src/terraria/run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-chmod +x TerrariaServer.bin.x86_64
 ./versions/$1/TerrariaServer.bin.x86_64 -config ./servers/$2
